a9fdf8e73c125ec7400c877abaa0dcd7221a716e
karol82 <karol82@int.pl>
2021-10-13 23:02:15 +0200

    remove duplicate, convert dict -> list, join chars to string

---
+++ counting-duplicates.py
@@ -0,0 +1,36 @@
+# Count the number of Duplicates
+# Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that
+# occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+#
+# Example
+# "abcde" -> 0 # no characters repeats more than once
+# "aabbcde" -> 2 # 'a' and 'b'
+# "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+# "indivisibility" -> 1 # 'i' occurs six times
+# "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+# "aA11" -> 2 # 'a' and '1'
+# "ABBA" -> 2 # 'A' and 'B' each occur twice
+
+def duplicate_count(text):
+    text = text.lower()
+    text_unique = list(dict.fromkeys(text))
+    text_unique = ''.join(text_unique)
+    how_many = 0
+    for each in text_unique:
+        temp = 0
+        for _ in text:
+            if _ == each:
+                temp += 1
+        if temp >= 2:
+            how_many += 1
+    return how_many
+
+
+
+test = 'Indivisibilities'
+print(duplicate_count(test))
+
+
+# BEST
+# def duplicate_count(s):
+#   return len([c for c in set(s.lower()) if s.lower().count(c)>1])
