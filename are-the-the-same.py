c6581e33a2e67899c655e5af44116401290889d0
karol82 <karol82@int.pl>
2021-10-15 23:56:31 +0200

    *args, loop in array, try except TypeError

--- are-the-the-same.py
+++ are-the-the-same.py
@@ -9,18 +9,23 @@
 # the square of 19, 25921 the square of 161, and so on. It gets obvious if we write b's elements in terms of squares:
 
 def comp(*args):
-    array1 = args[0]
-    if len(args) < 2 or type(args[1]) == 'NoneType':
-        return False
-    array2 = args[1]
-    print(array1)
-    print(array2)
-    array1 = [_ * _ for _ in array1]
-    array1.sort()
-    array2.sort()
-    if array1 == array2:
-        return True
-    else:
+    try:
+        if len(args) < 2 or type(args[1]) == 'NoneType':
+            return False
+        array1 = args[0]
+        print(type(args[1]))
+
+        array2 = args[1]
+        print(array1)
+        print(args[1])
+        array1 = [_ * _ for _ in array1]
+        array1.sort()
+        array2.sort()
+        if array1 == array2:
+            return True
+        else:
+            return False
+    except (AttributeError, TypeError):
         return False
 
 
